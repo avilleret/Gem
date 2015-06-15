3cdefa5dea60e2ee3095961d385ad72228f09fcd
Antoine Villeret <antoine.villeret@gmail.com>
2015-06-16 01:50:44 +0200

    add a BlackMagicDesign texin plugin

---
+++ plugins/texinBlackMagicDesign/texinBlackMagicDesign.cpp
@@ -0,0 +1,166 @@
+#include "texinBlackMagicDesign.h"
+#include "plugins/PluginFactory.h"
+
+#include <m_pd.h>
+
+using namespace gem::plugins;
+
+REGISTER_TEXINFACTORY("BlackMagicDesign", texinBlackMagicDesign);
+
+
+texinBlackMagicDesign::texinBlackMagicDesign(void)
+  : m_name(std::string("BlackMagicDesign")),  m_textureObj(0)
+{
+}
+
+texinBlackMagicDesign::~texinBlackMagicDesign(void) {
+  close();
+}
+
+void texinBlackMagicDesign::close(void) {
+}
+
+bool texinBlackMagicDesign::open(gem::Properties&props) {
+  /*
+  if(m_devname.empty())return false;
+  setProperties(props);
+  if(m_client)close();
+  m_client=rfbGetClient(8,3,4);
+  if(!m_client) {
+    return false;
+  }
+
+  m_client->GotFrameBufferUpdate = frameBufferCB;
+  m_client->GetPassword          = passwordCB;
+  rfbClientSetClientData(m_client, (void*)(rfb2gem), this);
+
+  if(true) {
+    char*devname=strdup(m_devname.c_str());
+    char*argv[]={
+      "gem", // fake program name
+      devname // the connection string
+    };
+    int argc=sizeof(argv)/sizeof(*argv);
+    rfbBool res=rfbInitClient(m_client, &argc, argv);
+    free(devname);
+    if(!res) {
+      // rfbInitClient() will call rfbClientCleanup() on failure automatically!
+      m_client=0;
+      return false;
+    }
+  }
+  */
+  return true;
+}
+
+GLuint texinBlackMagicDesign::getFrame(void) {
+  return m_textureObj;
+}
+
+std::vector<std::string>texinBlackMagicDesign::enumerate(void) {
+  std::vector<std::string>result;
+  // result.push_back("vnc");
+  return result;
+}
+
+bool texinBlackMagicDesign::setDevice(int ID) {
+  m_devname.clear();
+  return false;
+}
+bool texinBlackMagicDesign::setDevice(std::string device) {
+  m_devname.clear();
+  // const std::string prefix="vnc://";
+  /*
+  if (!device.compare(0, prefix.size(), prefix)) {
+    m_devname=device.substr(prefix.size());
+    //post("VNC: device '%s'", m_devname.c_str());
+    return true;
+  }
+  */
+  return false;
+}
+bool texinBlackMagicDesign::enumProperties(gem::Properties&readable,
+			       gem::Properties&writeable) {
+  std::string dummy_s;
+  int dummy_i=0;
+  readable.clear();
+  writeable.clear();
+
+  /*
+  readable.set("width", m_pixBlock.image.xsize);
+  readable.set("height", m_pixBlock.image.ysize);
+
+  writeable.set("password", dummy_s);
+
+  writeable.set("mouse.x", dummy_i);
+  writeable.set("mouse.y", dummy_i);
+  writeable.set("mouse.mask", dummy_i);
+  */
+
+  return true;
+}
+void texinBlackMagicDesign::setProperties(gem::Properties&props) {
+  m_props=props;
+
+  bool doMouse=false;
+
+  double num;
+  std::string s;
+
+  /*
+  if(props.get("password", s)) {
+    m_password=s;
+  }
+  if(props.get("mouse.x", num)) {
+    doMouse=true;
+    m_mouse.x=num;
+  }
+  if(props.get("mouse.y", num)) {
+    doMouse=true;
+    m_mouse.y=num;
+  }
+  if(props.get("mouse.mask", num)) {
+    doMouse=true;
+    m_mouse.mask=num;
+  }
+  */
+
+  /*
+  if(doMouse && m_client) {
+    if ((m_mouse.x != -1) && (m_mouse.y != -1)) {
+      SendPointerEvent 	(m_client, m_mouse.x, m_mouse.y, m_mouse.mask);
+      m_mouse.x = -1; m_mouse.y = -1;
+    }
+  }
+  */
+}
+void texinBlackMagicDesign::getProperties(gem::Properties&props) {
+  std::vector<std::string>keys=props.keys();
+  unsigned int i;
+  for(i=0; i<keys.size(); i++) {
+    if("width"==keys[i]) {
+
+    }
+    if("height"==keys[i]) {
+
+    }
+  }
+}
+
+/*
+std::vector<std::string>texinBlackMagicDesign::dialogs(void) {
+  std::vector<std::string>result;
+  return result;
+}
+*/
+bool texinBlackMagicDesign::provides(const std::string name) {
+  return (name==m_name);
+}
+std::vector<std::string>texinBlackMagicDesign::provides(void) {
+  std::vector<std::string>result;
+  result.push_back(m_name);
+  return result;
+}
+const std::string texinBlackMagicDesign::getName(void) {
+  return m_name;
+}
---
+++ plugins/texinBlackMagicDesign/texinBlackMagicDesign.h
@@ -0,0 +1,59 @@
+#ifndef _INCLUDE_GEMPLUGIN__VIDEOVNC_VIDEOVNC_H_
+#define _INCLUDE_GEMPLUGIN__VIDEOVNC_VIDEOVNC_H_
+
+#include "plugins/texin.h"
+#include "Gem/Image.h"
+
+
+#include <DeckLinkAPI.h>
+
+namespace gem { namespace plugins {
+    class GEM_EXPORT texinBlackMagicDesign : public texin {
+ private:
+   std::string m_name; // 'BlackMagicDesign'
+   std::string m_devname;
+
+   Properties m_props;
+
+   GLuint m_textureObj;
+
+ public:
+   texinBlackMagicDesign(void);
+
+   virtual ~texinBlackMagicDesign(void);
+
+   virtual bool open(gem::Properties&props);
+   virtual GLuint getFrame(void);
+
+   virtual std::vector<std::string>enumerate(void);
+   virtual bool	setDevice(int ID);
+   virtual bool	setDevice(const std::string);
+
+
+   virtual bool enumProperties(gem::Properties&readable,
+			       gem::Properties&writeable);
+   virtual void setProperties(gem::Properties&props);
+   virtual void getProperties(gem::Properties&props);
+
+   // for pix_video: query whether this backend provides access to this class of devices
+   // (e.g. "dv")
+   virtual bool provides(const std::string);
+   // get a list of all provided devices
+   virtual std::vector<std::string>provides(void);
+
+   // get's the name of the backend (e.g. "BlackMagicDesign")
+   virtual const std::string getName(void);
+
+   virtual bool isThreadable(void) {return false; return true;}
+   virtual bool reset(void) {return true;}
+   virtual void releaseFrame(void) {}
+   virtual bool grabAsynchronous(bool) {return false; return true;}
+   virtual bool setColor(int) {return false;}
+
+   virtual void close(void);
+   virtual bool start(void) {return true;};
+   virtual bool stop(void)  {return true;};
+};
+};}; // namespace
+
+#endif	// for header file
